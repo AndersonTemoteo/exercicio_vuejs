3a67aad389af75aa049ab42b686a2a4cf4edd2ea
AndersonTemoteo <andersontemoteobezerra@gmail.com>
2024-05-28 02:27:08 -0300

    resolução do exercício

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,53 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from 'vue';
+
+  const estado = reactive({
+    numeroUm: 0,
+    nummeroDois: 0,
+    resultado: 0,
+    filtro: 'soma',
+  })
+
+  const fazCalculo = () => {
+    const {filtro} = estado;
+
+    switch (filtro) {
+      case 'subtracao':
+        return parseFloat(estado.numeroUm) - parseFloat(estado.nummeroDois);
+      case 'multiplicacao':
+        return parseFloat(estado.numeroUm) * parseFloat(estado.nummeroDois);
+      case 'divisao':
+        return parseFloat(estado.numeroUm) / parseFloat(estado.nummeroDois);
+      default:
+        return parseFloat(estado.numeroUm) + parseFloat(estado.nummeroDois);
+    }
+  }
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container p-5 mb-4 mt-4 bg-light rounded-3">
+    <h1>Calculadora Aritmética</h1>
+    <form>
+      <div class="row pt-3">
+        <input type="number" required placeholder="Digite o primeiro número" class="form-control" @change="evento => estado.numeroUm = evento.target.value">
+      </div>
+      <div class="row pt-3">
+        <input type="number" required placeholder="Digite o segundo número" class="form-control" @change="evento => estado.nummeroDois = evento.target.value">
+      </div>
+      <div class="row pt-3">
+        <select @change="evento => estado.filtro = evento.target.value" class="form-control">
+          <option value="soma">Soma ( + )</option>
+          <option value="subtracao">Subtração ( - )</option>
+          <option value="multiplicacao">Multiplicação ( * )</option>
+          <option value="divisao">Divisão ( / )</option>
+        </select>
+        <span class="pt-4 fw-semibold fs-4"> Resultado: {{ fazCalculo() }}</span>
+      </div>
+    </form>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
